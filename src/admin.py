fd8a1ff740243872e333f99a07e5b864f6e41165
NahuelPiecora <nahuelpie@live.com>
2022-08-13 14:55:54 +0000

    updated

--- src/admin.py
+++ src/admin.py
@@ -15,5 +15,6 @@ def setup_admin(app):
     admin.add_view(ModelView(Character, db.session))
     admin.add_view(ModelView(FavoritePlanet, db.session))
     admin.add_view(ModelView(FavoriteCharacter, db.session))
+
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
